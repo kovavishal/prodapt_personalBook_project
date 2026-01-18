b2272b7ec468bb781d968134331d3e50c351261b
kovavishal <46000257+kovavishal@users.noreply.github.com>
2026-01-18 20:05:04 +0530

    modified code as per the task

--- src/test/java/com/example/demo/BookControllerTests.java
+++ src/test/java/com/example/demo/BookControllerTests.java
@@ -2,16 +2,24 @@ package com.example.demo;
 
 import com.example.demo.db.Book;
 import com.example.demo.db.BookRepository;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,6 +33,7 @@ class BookControllerTests {
     private WebApplicationContext context;
     @Autowired
     private BookRepository bookRepository;
+    static MockWebServer mockWebServer;
 
     @BeforeEach
     void setup() {
@@ -33,11 +42,77 @@ class BookControllerTests {
         bookRepository.save(new Book("12muzgEACAAJ", "Effective Java", "Joshua Bloch"));
     }
 
+    @BeforeAll
+    static void startMockServer() throws Exception {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterAll
+    static void stopMockServer() throws Exception {
+        mockWebServer.shutdown();
+    }
+
+    @DynamicPropertySource
+    static void overrideGoogleBaseUrl(DynamicPropertyRegistry registry) {
+        registry.add(
+                "google.books.base-url",
+                () -> mockWebServer.url("/").toString()
+        );
+    }
+
     @Test
     void testGetAllBooks() throws Exception {
         mockMvc.perform(get("/books"))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$[0].title").value("Spring in Action"))
             .andExpect(jsonPath("$[1].title").value("Effective Java"));
+    }
+
+
+    @Test
+    void testSaveBookFromGoogleApi() throws Exception {
+
+        String googleResponse = """
+            {
+              "id": "piOyzYqeZGgC",
+              "volumeInfo": {
+                "title": "Effective Java",
+                "authors": ["Joshua Bloch"],
+                "pageCount": 265
+              }
+            }
+            """;
+
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setResponseCode(200)
+                        .setHeader("Content-Type", "application/json")
+                        .setBody(googleResponse)
+        );
+
+        mockMvc.perform(post("/books/piOyzYqeZGgC"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value("piOyzYqeZGgC"))
+                .andExpect(jsonPath("$.title").value("Effective Java"))
+                .andExpect(jsonPath("$.author").value("Joshua Bloch"));
+
+        assertThat(bookRepository.count()).isEqualTo(3);
+    }
+
+    @Test
+    void testSaveBookFailsWhenGoogleResponseInvalid() throws Exception {
+
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setResponseCode(200)
+                        .setHeader("Content-Type", "application/json")
+                        .setBody("{}")
+        );
+
+        mockMvc.perform(post("/books/invalid-id"))
+                .andExpect(status().isBadRequest());
+
+        assertThat(bookRepository.count()).isEqualTo(2);
     }
 }
